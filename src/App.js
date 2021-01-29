47cc6e9e4470292f8dd66ff9f918c8f3e2110370
it17150644 <it17150644@my.sliit.lk>
2021-01-29 07:26:11 +0530

    app project first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useState } from "react";
 
 function App() {
+  const [glob, setGlob] = useState("");
+
+  const getDetails = () => {
+    axios
+      .get("https://www.hpb.health.gov.lk/api/get-current-statistical")
+      .then((response) => {
+        console.log(response);
+      });
+  };
+
+  // view in webpage
+
+  const getDetailsMore = () => {
+    axios
+      .get("https://www.hpb.health.gov.lk/api/get-current-statistical")
+      .then((response) => {
+        setGlob(" අද දින ලාංකික තතු.. " + " ගෝලිය = " + response.data.data.global_new_cases  + " ලාංකික අලුත් = " + response.data.data.local_new_cases + " ලාංකික සියලු =" +response.data.data.local_total_cases +  " ලාංකික මරණ =" +response.data.data.local_deaths + " නවතම මරණ =  " + response.data.data.local_new_deaths + " 🛌  " + " හොදවු සංඛයාව =" +response.data.data.local_recovered);
+      });
+  };
+
+
+  // sl Count 
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>COVID-19 Detials in Sri Lanka</h1>
+      <button onClick={getDetails}>ALL Details On Your Console</button>
+      <hr />
+
+      <br />
+      <button onClick={getDetailsMore}>ලාංකික කොරෝනා තතු සදහා ...</button>
+      <h2>{glob}</h2>
+
+      <br/>
+      {/* <button onClick={getDetailsSl}>Sri lanka Corona Details</button>
+      <h2>{sl}</h2> */}
     </div>
   );
 }
